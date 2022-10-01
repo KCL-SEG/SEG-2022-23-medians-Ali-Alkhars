e6721ee3e75454ee1db591194299cf1a7f1af31e
Ali Alkhars <a-khars@hotmail.com>
2022-10-01 18:01:45 +0100

    fix bug

--- median.py
+++ median.py
@@ -12,8 +12,8 @@ while True:
         if len(numbers) % 2 != 0:
             median = numbers[len(numbers)//2]
         else:
-            middleIndx = numbers[len(numbers)//2]
-            median = (middleIndx + middleIndx-1) / 2
+            middleIndx = len(numbers)//2
+            median = (numbers[middleIndx] + numbers[middleIndx-1]) / 2
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
